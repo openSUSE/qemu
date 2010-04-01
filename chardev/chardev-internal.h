4910c6897c2594e22674797a629e229c7c1def1c
Alexander Graf <agraf@suse.de>
2010-04-01 17:36:23 +0200

    [openSUSE] Make char muxer more robust wrt small FIFOs
    Virtio-Console can only process one character at a time. Using it on S390
    gave me strange "lags" where I got the character I pressed before when
    pressing one. So I typed in "abc" and only received "a", then pressed "d"
    but the guest received "b" and so on.
    
    While the stdio driver calls a poll function that just processes on its
    queue in case virtio-console can't take multiple characters at once, the
    muxer does not have such callbacks, so it can't empty its queue.
    
    To work around that limitation, I introduced a new timer that only gets
    active when the guest can not receive any more characters. In that case
    it polls again after a while to check if the guest is now receiving input.
    
    This patch fixes input when using -nographic on s390 for me.
    
    [AF: Rebased for v2.7.0-rc2]
    [BR: minor edits to pass qemu's checkpatch script]
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- chardev/chardev-internal.h
+++ chardev/chardev-internal.h
@@ -41,6 +41,9 @@ struct MuxChardev {
     /* frontend of the underlying muxed chardev */
     CharFrontend chr;
     unsigned long mux_bitset;
+#if defined(TARGET_S390X)
+    QEMUTimer *accept_timer;
+#endif
     int focus;
     bool term_got_escape;
     /* Intermediate input buffer catches escape sequences even if the
