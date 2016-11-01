f7b5ea7211cc3b29d99d305cf49b84bc8901c31c
Greg Kurz <groug@kaod.org>
2016-11-01 12:00:40 +0100

    9pfs: drop excessive error message from virtfs_reset()
    The virtfs_reset() function is called either when the virtio-9p device
    gets reset, or when the client starts a new 9P session. In both cases,
    if it finds fids from a previous session, the following is printed in
    the monitor:
    
    9pfs:virtfs_reset: One or more uncluncked fids found during reset
    
    For example, if a linux guest with a mounted 9P share is reset from the
    monitor with system_reset, the message will be printed. This is excessive
    since these fids are now clunked and the state is clean.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    (cherry picked from commit 79decce35b4d769fa878b048ab1a7b3e9045c9c6)
    [BR: Fix and/or infrastructure for BSC#1020427 CVE-2016-9602]
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -531,7 +531,7 @@ static int v9fs_mark_fids_unreclaim(V9fsPDU *pdu, V9fsPath *path)
 static void virtfs_reset(V9fsPDU *pdu)
 {
     V9fsState *s = pdu->s;
-    V9fsFidState *fidp = NULL;
+    V9fsFidState *fidp;
 
     /* Free all fids */
     while (s->fid_list) {
@@ -543,11 +543,6 @@ static void virtfs_reset(V9fsPDU *pdu)
         } else {
             free_fid(pdu, fidp);
         }
-    }
-    if (fidp) {
-        /* One or more unclunked fids found... */
-        error_report("9pfs:%s: One or more uncluncked fids "
-                     "found during reset", __func__);
     }
 }
 
