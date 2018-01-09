5208f5893f37eb80b26eb31ff0ea2a5f2600ffdc
Eduardo Habkost <ehabkost@redhat.com>
2018-01-09 13:45:16 -0200

    i386: Add FEAT_8000_0008_EBX CPUID feature word
    Add the new feature word and the "ibpb" feature flag.
    
    Based on a patch by Paolo Bonzini.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Message-Id: <20180109154519.25634-5-ehabkost@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    (cherry picked from commit 1b3420e1c4d523c49866cca4e7544753201cd43d)
    [BR: BSC#1068032 CVE-2017-5715 change to match current code's feat_name
    type]
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- target-i386/cpu.h
+++ target-i386/cpu.h
@@ -439,6 +439,7 @@ typedef enum FeatureWord {
     FEAT_8000_0001_EDX, /* CPUID[8000_0001].EDX */
     FEAT_8000_0001_ECX, /* CPUID[8000_0001].ECX */
     FEAT_8000_0007_EDX, /* CPUID[8000_0007].EDX */
+    FEAT_8000_0008_EBX, /* CPUID[8000_0008].EBX */
     FEAT_C000_0001_EDX, /* CPUID[C000_0001].EDX */
     FEAT_KVM,           /* CPUID[4000_0001].EAX (KVM_CPUID_FEATURES) */
     FEAT_SVM,           /* CPUID[8000_000A].EDX */
@@ -610,6 +611,8 @@ typedef uint32_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_7_0_ECX_PKU      (1U << 3)
 #define CPUID_7_0_ECX_OSPKE    (1U << 4)
 #define CPUID_7_0_EDX_SPEC_CTRL     (1U << 26) /* Speculation Control */
+
+#define CPUID_8000_0008_EBX_IBPB    (1U << 12) /* Indirect Branch Prediction Barrier */
 
 #define CPUID_XSAVE_XSAVEOPT   (1U << 0)
 #define CPUID_XSAVE_XSAVEC     (1U << 1)
