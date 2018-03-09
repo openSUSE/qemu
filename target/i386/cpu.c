689e263e1ee30286bb4717a65fc22c4c8a282302
Bruce Rogers <brogers@suse.com>
2018-03-09 05:27:44 -0700

    x86: cpu: increase model_id array size from 48 to 49
    This is perhaps the easiest way to handle the longer model names
    introduced with the -IBRS models. This is in lew of backporting
    commit 807e9869b8c4119b81df902625af818519e01759 and other commits
    which that would require.
    [BR: BSC#1068032 CVE-2017-5715]
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -753,7 +753,7 @@ struct X86CPUDefinition {
     int model;
     int stepping;
     FeatureWordArray features;
-    char model_id[48];
+    char model_id[49];
 };
 
 static X86CPUDefinition builtin_x86_defs[] = {
