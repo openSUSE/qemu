ceb1f66574494bed4f2dbe02bf102c1de36870dc
Cornelia Huck <cohuck@redhat.com>
2020-04-27 12:24:14 +0200

    linux-headers: update against Linux 5.7-rc3
    Git-commit: dc6f8d458a4ccc360723993f31d310d06469f55f
    References: bsc#1179719
    
    commit 6a8b55ed4056ea5559ebe4f6a4b247f627870d4c
    
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com> # virtio/vhost parts
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    Message-Id: <20200427102415.10915-3-cohuck@redhat.com>
    Signed-off-by: Liang Yan <lyan@suse.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -1016,6 +1016,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_ARM_INJECT_EXT_DABT 178
 #define KVM_CAP_S390_VCPU_RESETS 179
 #define KVM_CAP_S390_PROTECTED 180
+#define KVM_CAP_PPC_SECURE_GUEST 181
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
@@ -1668,5 +1669,8 @@ struct kvm_hyperv_eventfd {
 
 #define KVM_HYPERV_CONN_ID_MASK		0x00ffffff
 #define KVM_HYPERV_EVENTFD_DEASSIGN	(1 << 0)
+
+#define KVM_DIRTY_LOG_MANUAL_PROTECT_ENABLE    (1 << 0)
+#define KVM_DIRTY_LOG_INITIALLY_SET            (1 << 1)
 
 #endif /* __LINUX_KVM_H */
