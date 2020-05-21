ba8402f5c64c095e7f349126d122136d8dc20247
BALATON Zoltan <balaton@eik.bme.hu>
2020-05-21 21:39:44 +0200

    sm501: Convert printf + abort to qemu_log_mask
    Git-commit: e29da77e5fddf6480e3a0e80b63d703edaec751b
    References: bsc#1172385, CVE-2020-12829
    
    Some places already use qemu_log_mask() to log unimplemented features
    or errors but some others have printf() then abort(). Convert these to
    qemu_log_mask() and avoid aborting to prevent guests to easily cause
    denial of service.
    
    Signed-off-by: BALATON Zoltan <balaton@eik.bme.hu>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-id: 305af87f59d81e92f2aaff09eb8a3603b8baa322.1590089984.git.balaton@eik.bme.hu
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- hw/display/sm501.c
+++ hw/display/sm501.c
@@ -693,8 +693,8 @@ static void sm501_2d_operation(SM501State * s)
     int fb_len = get_width(s, crt) * get_height(s, crt) * get_bpp(s, crt);
 
     if (addressing != 0x0) {
-        printf("%s: only XY addressing is supported.\n", __func__);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: only XY addressing is supported.\n");
+        return;
     }
 
     if (rop_mode == 0) {
@@ -720,8 +720,8 @@ static void sm501_2d_operation(SM501State * s)
 
     if ((s->twoD_source_base & 0x08000000) ||
         (s->twoD_destination_base & 0x08000000)) {
-        printf("%s: only local memory is supported.\n", __func__);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: only local memory is supported.\n");
+        return;
     }
 
     switch (operation) {
@@ -787,9 +787,9 @@ static void sm501_2d_operation(SM501State * s)
         break;
 
     default:
-        printf("non-implemented SM501 2D operation. %d\n", operation);
-        abort();
-        break;
+        qemu_log_mask(LOG_UNIMP, "sm501: not implemented 2D operation: %d\n",
+                      operation);
+        return;
     }
 
     if (dst_base >= get_fb_addr(s, crt) &&
@@ -852,9 +852,8 @@ static uint64_t sm501_system_config_read(void *opaque, hwaddr addr,
         break;
 
     default:
-	printf("sm501 system config : not implemented register read."
-	       " addr=%x\n", (int)addr);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: not implemented system config"
+                      "register read. addr=%" HWADDR_PRIx "\n", addr);
     }
 
     return ret;
@@ -908,9 +907,9 @@ static void sm501_system_config_write(void *opaque, hwaddr addr,
         break;
 
     default:
-	printf("sm501 system config : not implemented register write."
-	       " addr=%x, val=%x\n", (int)addr, (uint32_t)value);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: not implemented system config"
+                      "register write. addr=%" HWADDR_PRIx
+                      ", val=%" PRIx64 "\n", addr, value);
     }
 }
 
@@ -1040,9 +1039,8 @@ static uint64_t sm501_disp_ctrl_read(void *opaque, hwaddr addr,
         break;
 
     default:
-	printf("sm501 disp ctrl : not implemented register read."
-	       " addr=%x\n", (int)addr);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: not implemented disp ctrl register "
+                      "read. addr=%" HWADDR_PRIx "\n", addr);
     }
 
     return ret;
@@ -1172,9 +1170,9 @@ static void sm501_disp_ctrl_write(void *opaque, hwaddr addr,
         break;
 
     default:
-	printf("sm501 disp ctrl : not implemented register write."
-	       " addr=%x, val=%x\n", (int)addr, (unsigned)value);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: not implemented disp ctrl register "
+                      "write. addr=%" HWADDR_PRIx
+                      ", val=%" PRIx64 "\n", addr, value);
     }
 }
 
@@ -1200,9 +1198,8 @@ static uint64_t sm501_2d_engine_read(void *opaque, hwaddr addr,
         ret = s->twoD_source_base;
         break;
     default:
-        printf("sm501 disp ctrl : not implemented register read."
-               " addr=%x\n", (int)addr);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: not implemented disp ctrl register "
+                      "read. addr=%" HWADDR_PRIx "\n", addr);
     }
 
     return ret;
@@ -1260,9 +1257,9 @@ static void sm501_2d_engine_write(void *opaque, hwaddr addr,
         s->twoD_destination_base = value;
         break;
     default:
-        printf("sm501 2d engine : not implemented register write."
-               " addr=%x, val=%x\n", (int)addr, (unsigned)value);
-        abort();
+        qemu_log_mask(LOG_UNIMP, "sm501: not implemented 2d engine register "
+                      "write. addr=%" HWADDR_PRIx
+                      ", val=%" PRIx64 "\n", addr, value);
     }
 }
 
@@ -1411,8 +1408,9 @@ static void sm501_update_display(void *opaque)
         draw_line = draw_line32_funcs[ds_depth_index];
         break;
     default:
-        printf("sm501 update display : invalid control register value.\n");
-        abort();
+        qemu_log_mask(LOG_GUEST_ERROR, "sm501 draw crt"
+		      "invalid DC_CRT_CONTROL=%x.\n",
+	       	      s->dc_crt_control);
 	break;
     }
 
