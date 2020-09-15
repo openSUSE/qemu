8d9b7614375f0f887b6b55aad597ad3f8708031b
Prasad J Pandit <pjp@fedoraproject.org>
2020-09-15 23:52:59 +0530

    hw: usb: hcd-ohci: check for processed TD before retire
    Git-commit: 1be90ebecc95b09a2ee5af3f60c412b45a766c4f
    References: bsc#1176684, CVE-2020-25625
    
    While servicing OHCI transfer descriptors(TD), ohci_service_iso_td
    retires a TD if it has passed its time frame. It does not check if
    the TD was already processed once and holds an error code in TD_CC.
    It may happen if the TD list has a loop. Add check to avoid an
    infinite loop condition.
    
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Reviewed-by: Li Qiang <liq3ea@gmail.com>
    Message-id: 20200915182259.68522-3-ppandit@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- hw/usb/hcd-ohci.c
+++ hw/usb/hcd-ohci.c
@@ -734,6 +734,10 @@ static int ohci_service_iso_td(OHCIState *ohci, struct ohci_ed *ed,
            the next ISO TD of the same ED */
         DPRINTF("usb-ohci: ISO_TD R=%d > FC=%d\n", relative_frame_number, 
                frame_count);
+        if (OHCI_CC_DATAOVERRUN == OHCI_BM(iso_td.flags, TD_CC)) {
+            /* avoid infinite loop */
+            return 1;
+        }
         OHCI_SET_BM(iso_td.flags, TD_CC, OHCI_CC_DATAOVERRUN);
         ed->head &= ~OHCI_DPTR_MASK;
         ed->head |= (iso_td.next & OHCI_DPTR_MASK);
