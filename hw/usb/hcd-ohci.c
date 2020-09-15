c9149f3d0c0e029b546b468d59ab0ab85b9a058b
Prasad J Pandit <pjp@fedoraproject.org>
2020-09-15 23:52:58 +0530

    hw: usb: hcd-ohci: check len and frame_number variables
    Git-commit: 1328fe0c32d5474604105b8105310e944976b058
    References: bsc#1176682, CVE-2020-25624
    
    While servicing the OHCI transfer descriptors(TD), OHCI host
    controller derives variables 'start_addr', 'end_addr', 'len'
    etc. from values supplied by the host controller driver.
    Host controller driver may supply values such that using
    above variables leads to out-of-bounds access issues.
    Add checks to avoid them.
    
    AddressSanitizer: stack-buffer-overflow on address 0x7ffd53af76a0
      READ of size 2 at 0x7ffd53af76a0 thread T0
      #0 ohci_service_iso_td ../hw/usb/hcd-ohci.c:734
      #1 ohci_service_ed_list ../hw/usb/hcd-ohci.c:1180
      #2 ohci_process_lists ../hw/usb/hcd-ohci.c:1214
      #3 ohci_frame_boundary ../hw/usb/hcd-ohci.c:1257
      #4 timerlist_run_timers ../util/qemu-timer.c:572
      #5 qemu_clock_run_timers ../util/qemu-timer.c:586
      #6 qemu_clock_run_all_timers ../util/qemu-timer.c:672
      #7 main_loop_wait ../util/main-loop.c:527
      #8 qemu_main_loop ../softmmu/vl.c:1676
      #9 main ../softmmu/main.c:50
    
    Reported-by: Gaoning Pan <pgn@zju.edu.cn>
    Reported-by: Yongkang Jia <j_kangel@163.com>
    Reported-by: Yi Ren <yunye.ry@alibaba-inc.com>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Message-id: 20200915182259.68522-2-ppandit@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- hw/usb/hcd-ohci.c
+++ hw/usb/hcd-ohci.c
@@ -777,7 +777,11 @@ static int ohci_service_iso_td(OHCIState *ohci, struct ohci_ed *ed,
     }
 
     start_offset = iso_td.offset[relative_frame_number];
-    next_offset = iso_td.offset[relative_frame_number + 1];
+    if (relative_frame_number < frame_count) {
+        next_offset = iso_td.offset[relative_frame_number + 1];
+    } else {
+        next_offset = iso_td.be;
+    }
 
     if (!(OHCI_BM(start_offset, TD_PSW_CC) & 0xe) || 
         ((relative_frame_number < frame_count) && 
@@ -812,7 +816,11 @@ static int ohci_service_iso_td(OHCIState *ohci, struct ohci_ed *ed,
         }
     } else {
         /* Last packet in the ISO TD */
-        end_addr = iso_td.be;
+        end_addr = next_offset;
+    }
+
+    if (start_addr > end_addr) {
+        return 1;
     }
 
     if ((start_addr & OHCI_PAGE_MASK) != (end_addr & OHCI_PAGE_MASK)) {
@@ -820,6 +828,9 @@ static int ohci_service_iso_td(OHCIState *ohci, struct ohci_ed *ed,
             - (start_addr & OHCI_OFFSET_MASK);
     } else {
         len = end_addr - start_addr + 1;
+    }
+    if (len > sizeof(ohci->usb_buf)) {
+        len = sizeof(ohci->usb_buf);
     }
 
     if (len && dir != OHCI_TD_DIR_IN) {
@@ -988,7 +999,13 @@ static int ohci_service_td(OHCIState *ohci, struct ohci_ed *ed)
         if ((td.cbp & 0xfffff000) != (td.be & 0xfffff000)) {
             len = (td.be & 0xfff) + 0x1001 - (td.cbp & 0xfff);
         } else {
+            if (td.cbp > td.be) {
+                return 1;
+            }
             len = (td.be - td.cbp) + 1;
+        }
+        if (len > sizeof(ohci->usb_buf)) {
+            len = sizeof(ohci->usb_buf);
         }
 
         pktlen = len;
