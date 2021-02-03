b4086a4eba887415c7b93a92a578e53a7f4eb216
Bruce Rogers <brogers@suse.com>
2021-02-03 14:25:38 -0700

    virtio-scsi: change DID TIMEOUT handling
    This patch implements a change of SG_ERR_DID_TIME_OUT handling as
    suggested in
    https://bugzilla.suse.com/show_bug.cgi?id=1178049#c145
    
    Suggested-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- hw/scsi/virtio-scsi.c
+++ hw/scsi/virtio-scsi.c
@@ -507,7 +507,6 @@ static void virtio_scsi_command_complete(SCSIRequest *r, uint32_t status,
         req->resp.cmd.response = VIRTIO_SCSI_S_INCORRECT_LUN;
         break;
     case SG_ERR_DID_ABORT:
-    case SG_ERR_DID_TIME_OUT:
         req->resp.cmd.response = VIRTIO_SCSI_S_ABORTED;
         break;
     case SG_ERR_DID_BAD_TARGET:
@@ -517,6 +516,7 @@ static void virtio_scsi_command_complete(SCSIRequest *r, uint32_t status,
         req->resp.cmd.response = VIRTIO_SCSI_S_RESET;
         break;
     case SG_ERR_DID_BUS_BUSY:
+    case SG_ERR_DID_TIME_OUT:
         req->resp.cmd.response = VIRTIO_SCSI_S_BUSY;
         break;
     case SG_ERR_DID_TRANSPORT_DISRUPTED:
