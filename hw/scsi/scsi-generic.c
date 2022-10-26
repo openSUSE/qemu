1a9d21951565e0beefa05bdd5530d5feb17617e6
Hannes Reinecke <hare@suse.de>
2022-10-26 12:31:21 +0200

    hw/scsi/scsi-generic: Fixup VPD block limits emulation
    Move the check for SG_IO errors after the VPD block limits emulation,
    otherwise the emulation will never the triggered.
    
    References: bsc#1202364
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- hw/scsi/scsi-generic.c
+++ hw/scsi/scsi-generic.c
@@ -268,10 +268,7 @@ static void scsi_read_complete(void * opaque, int ret)
 
     aio_context_acquire(blk_get_aio_context(s->conf.blk));
 
-    if (ret || r->req.io_canceled ||
-        r->io_header.status ||
-        r->io_header.driver_status ||
-        r->io_header.host_status) {
+    if (ret || r->req.io_canceled) {
         scsi_command_complete_noio(r, ret);
         goto done;
     }
@@ -307,7 +304,9 @@ static void scsi_read_complete(void * opaque, int ret)
         }
     }
 
-    if (len == 0) {
+    if (len == 0 || r->io_header.status ||
+        r->io_header.driver_status ||
+        r->io_header.host_status) {
         scsi_command_complete_noio(r, 0);
         goto done;
     }
@@ -385,10 +384,7 @@ static void scsi_write_complete(void * opaque, int ret)
 
     aio_context_acquire(blk_get_aio_context(s->conf.blk));
 
-    if (ret || r->req.io_canceled ||
-        r->io_header.status ||
-        r->io_header.driver_status ||
-        r->io_header.host_status) {
+    if (ret || r->req.io_canceled) {
         scsi_command_complete_noio(r, ret);
         goto done;
     }
