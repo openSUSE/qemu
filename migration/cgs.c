acf8c03629717a4aa16f08d3f10f6dc9f579e890
Wei Wang <wei.w.wang@intel.com>
2022-11-02 18:50:31 +0800

    migration/cgs: add cgs_mig_init
    cgs_mig_init calls the vendor specific init function based the vm type
    to initialize the cgs_mig callbacks.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- migration/cgs.c
+++ migration/cgs.c
@@ -14,6 +14,7 @@
 #include "qemu/osdep.h"
 #include "qemu/error-report.h"
 #include "qemu-file.h"
+#include "sysemu/kvm.h"
 #include "savevm.h"
 #include "ram.h"
 #include "cgs.h"
@@ -207,4 +208,21 @@ void cgs_mig_loadvm_state_cleanup(void)
     }
 
     cgs_mig.loadvm_state_cleanup();
+}
+
+void cgs_mig_init(void)
+{
+    uint32_t nr_channels = 1;
+
+    if (migrate_use_multifd()) {
+        nr_channels = migrate_multifd_channels();
+    }
+
+    switch (kvm_vm_type) {
+    case KVM_X86_TDX_VM:
+        tdx_mig_init(&cgs_mig, nr_channels);
+        break;
+    default:
+        return;
+    }
 }
