a8eceefacaedb70a9193e8c58e5b8d63754f393b
Fabiano Rosas <farosas@suse.de>
2023-03-15 12:20:16 -0300

    migration/multifd: Add outgoing QIOChannelFile support
    Allow multifd to open file-backed channels. This will be used when
    enabling the fixed-ram migration stream format which expects a
    seekable transport.
    
    The QIOChannel read and write methods will use the preadv/pwritev
    versions which don't update the file offset at each call so we can
    reuse the fd without re-opening for every channel.
    
    Note that this is just setup code and multifd cannot yet make use of
    the file channels.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- migration/migration.c
+++ migration/migration.c
@@ -106,7 +106,7 @@ static bool migration_needs_multiple_sockets(void)
 static bool uri_supports_multi_channels(const char *uri)
 {
     return strstart(uri, "tcp:", NULL) || strstart(uri, "unix:", NULL) ||
-           strstart(uri, "vsock:", NULL);
+           strstart(uri, "vsock:", NULL) || strstart(uri, "file:", NULL);
 }
 
 static bool
