1f9e15b3fcaf208f18bc4a8732204b232cf3ba73
Fabiano Rosas <farosas@suse.de>
2023-05-08 17:24:04 -0300

    migration/ram: Ignore multifd flush when doing fixed-ram migration
    Some functionalities of multifd are incompatible with the 'fixed-ram'
    migration format.
    
    The MULTIFD_FLUSH flag in particular is not used because in fixed-ram
    there is no sinchronicity between migration source and destination so
    there is not need for a sync packet. In fact, fixed-ram disables
    packets in multifd as a whole.
    
    Make sure RAM_SAVE_FLAG_MULTIFD_FLUSH is never emitted when fixed-ram
    is enabled.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- migration/ram.c
+++ migration/ram.c
@@ -1386,15 +1386,13 @@ static int find_dirty_block(RAMState *rs, PageSearchStatus *pss)
         pss->page = 0;
         pss->block = QLIST_NEXT_RCU(pss->block, next);
         if (!pss->block) {
-            if (migrate_multifd() &&
+            if (!migrate_fixed_ram() && migrate_multifd() &&
                 !migrate_multifd_flush_after_each_section()) {
                 QEMUFile *f = rs->pss[RAM_CHANNEL_PRECOPY].pss_channel;
                 int ret = multifd_send_sync_main(f);
                 if (ret < 0) {
                     return ret;
                 }
-                qemu_put_be64(f, RAM_SAVE_FLAG_MULTIFD_FLUSH);
-                qemu_fflush(f);
             }
             /*
              * If memory migration starts over, we will meet a dirtied page
