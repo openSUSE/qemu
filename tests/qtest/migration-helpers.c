91db7ee50dae059fddebcc7803777e47667f2eea
Fabiano Rosas <farosas@suse.de>
2023-11-10 16:28:14 -0300

    tests/qtest/migration: Add a wrapper to print test names
    Our usage of gtest results in us losing the very basic functionality
    of "knowing which test failed". The issue is that gtest only prints
    test names ("paths" in gtest parlance) once the test has finished, but
    we use asserts in the tests and crash gtest itself before it can print
    anything. We also use a final abort when the result of g_test_run is
    not 0.
    
    Depending on how the test failed/broke we can see the function that
    trigged the abort, which may be representative of the test, but it
    could also just be some generic function.
    
    We have been relying on the primitive method of looking at the name of
    the previous successful test and then looking at the code to figure
    out which test should have come next.
    
    Add a wrapper to the test registration that does the job of printing
    the test name before running.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- tests/qtest/migration-helpers.c
+++ tests/qtest/migration-helpers.c
@@ -340,3 +340,35 @@ bool probe_o_direct_support(const char *tmpfs)
     return true;
 }
 #endif
+
+typedef struct {
+    char *name;
+    void (*func)(void);
+} MigrationTest;
+
+static void migration_test_destroy(gpointer data)
+{
+    MigrationTest *test = (MigrationTest *)data;
+
+    g_free(test->name);
+    g_free(test);
+}
+
+static void migration_test_wrapper(const void *data)
+{
+    MigrationTest *test = (MigrationTest *)data;
+
+    g_test_message("Running /%s%s", qtest_get_arch(), test->name);
+    test->func();
+}
+
+void migration_test_add(const char *path, void (*fn)(void))
+{
+    MigrationTest *test = g_new0(MigrationTest, 1);
+
+    test->func = fn;
+    test->name = g_strdup(path);
+
+    qtest_add_data_func_full(path, test, migration_test_wrapper,
+                             migration_test_destroy);
+}
