ecfc8715aeedaf506957201588ad8d440f92b967
Paolo Bonzini <pbonzini@redhat.com>
2024-07-03 11:29:17 +0200

    target/i386: do not include undefined bits in the AMD topoext leaf
    Commit d7c72735f61 ("target/i386: Add new EPYC CPU versions with updated
    cache_info", 2023-05-08) ensured that AMD-defined CPU models did not
    have the 'complex_indexing' bit set, but left it set in "-cpu host"
    which uses the default ("legacy") cache information.
    
    Reimplement that commit using a CPU feature, so that it can be applied
    to all guests using a new machine type, independent of the CPU model.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -6650,6 +6650,9 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
             *eax = *ebx = *ecx = *edx = 0;
             break;
         }
+        if (cpu->amd_topoext_features_only) {
+            *edx &= CACHE_NO_INVD_SHARING | CACHE_INCLUSIVE;
+        }
         break;
     case 0x8000001E:
         if (cpu->core_id <= 255) {
@@ -7919,6 +7922,7 @@ static Property x86_cpu_properties[] = {
     DEFINE_PROP_STRING("hv-vendor-id", X86CPU, hyperv_vendor),
     DEFINE_PROP_BOOL("cpuid-0xb", X86CPU, enable_cpuid_0xb, true),
     DEFINE_PROP_BOOL("x-vendor-cpuid-only", X86CPU, vendor_cpuid_only, true),
+    DEFINE_PROP_BOOL("x-amd-topoext-features-only", X86CPU, amd_topoext_features_only, true),
     DEFINE_PROP_BOOL("lmce", X86CPU, enable_lmce, false),
     DEFINE_PROP_BOOL("l3-cache", X86CPU, enable_l3_cache, true),
     DEFINE_PROP_BOOL("kvm-no-smi-migration", X86CPU, kvm_no_smi_migration,
