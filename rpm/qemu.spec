openSUSE/qemu

commit dec5f6c8a7acd23222a14c6600d6967219fda65c

[openSUSE][RPM] Normalize hostname, for reproducible builds
Use a fixed USER value (in case someone builds outside of OBS/osc).

References: boo#1084909
Signed-off-by: Bernhard M. Wiedemann <githubbmwprimary@lsmod.de>
Signed-off-by: Dario Faggioli <dfaggioli@suse.com>

rpm/qemu.spec
@@ -839,6 +839,8 @@ done
 
 %install
 cd %blddir
+export USER=abuild
+export HOSTNAME=OBS # is used in roms/SLOF/Makefile.gen (boo#1084909)
 
 %make_build install DESTDIR=%{buildroot}
 

commit d2a462340ae7a45ab52a826573fee9df92330216

[openSUSE][RPM] Hostname normalization (for repr. build ) done properly (boo#1084909)
In commit "[openSUSE][RPM] Normalize hostname, for reproducible builds"
(dec5f6c8a7acd23222a14c6600d6967219fda65c) the USER and HOSTNAME
variables were defined in the different RPM section. Fix that.

Fixes: dec5f6c8a7acd23222a14c6600d6967219fda65c
References: boo#1084909
Suggested-by: Bernhard M. Wiedemann <githubbmwprimary@lsmod.de>
Signed-offf-by: Dario Faggioli <dfaggioli@suse.com>

rpm/qemu.spec
@@ -498,6 +498,8 @@ find . -iname ".git" -exec rm -rf {} +
 
 mkdir -p %blddir
 cd %blddir
+export USER=abuild
+export HOSTNAME=OBS # is used in roms/SLOF/Makefile.gen (boo#1084909)
 
 # We define a few general and common options and then we disable
 # pretty much everything. Afterwards, there is a section for each
@@ -904,8 +906,6 @@ done
 
 %install
 cd %blddir
-export USER=abuild
-export HOSTNAME=OBS # is used in roms/SLOF/Makefile.gen (boo#1084909)
 
 %make_build install DESTDIR=%{buildroot}
 