bfa35094ae83c241b85b499d744da609b409f500
Xiaoyao Li <xiaoyao.li@intel.com>
2024-08-02 02:57:01 -0400

    i386/cpu: Introduce enable_cpuid_0x1f to force exposing CPUID 0x1f
    Currently, QEMU exposes CPUID 0x1f to guest only when necessary, i.e.,
    when topology level that cannot be enumerated by leaf 0xB, e.g., die or
    module level, are configured for the guest, e.g., -smp xx,dies=2.
    
    However, TDX architecture forces to require CPUID 0x1f to configure CPU
    topology.
    
    Introduce a bool flag, enable_cpuid_0x1f, in CPU for the case that
    requires CPUID leaf 0x1f to be exposed to guest.
    
    Introduce a new function x86_has_cpuid_0x1f(), which is the warpper of
    cpu->enable_cpuid_0x1f and x86_has_extended_topo() to check if it needs
    to enable cpuid leaf 0x1f for the guest.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -2034,6 +2034,9 @@ struct ArchCPU {
     /* Compatibility bits for old machine types: */
     bool enable_cpuid_0xb;
 
+    /* Force to enable cpuid 0x1f */
+    bool enable_cpuid_0x1f;
+
     /* Enable auto level-increase for all CPUID leaves */
     bool full_cpuid_auto_level;
 
@@ -2326,6 +2329,12 @@ static inline uint32_t host_cpu_phys_bits(void)
     return 40; // TCG_PHYS_ADDR_BITS
 }
 #endif
+
+static inline bool x86_has_cpuid_0x1f(X86CPU *cpu)
+{
+    return cpu->enable_cpuid_0x1f ||
+           x86_has_extended_topo(cpu->env.avail_cpu_topo);
+}
 
 /* helper.c */
 void x86_cpu_set_a20(X86CPU *cpu, int a20_state);
