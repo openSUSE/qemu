05701b119149ff83d4e958ac4db42a8620afd91f
Paolo Bonzini <pbonzini@redhat.com>
2025-08-19 17:02:35 +0200

    Revert "i386/cpu: Warn about why CPUID_EXT_PDCM is not available" (bsc#1271635)
    This reverts commit 00268e00027459abede448662f8794d78eb4b0a4.
    (The only conflict is in the !is_tdx_vm() part of the condition,
    which is safe to keep).
    
    mark_unavailable_features() actively blocks usage of the feature,
    so it is a functional change, not merely a emitting warning.
    The commit was intended to merely warn if PDCM was enabled when
    the performance counters are not, so revert it.
    
    Reported-by: Christian A. Ehrhardt <christian.ehrhardt@canonical.com>
    Analyzed-by: Daniel P. Berrangé <berrange@redhat.com>
    Analyzed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-ID: <20250819150235.785559-1-pbonzini@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit faaaf017d5b9c9f84fb86dcee016944176eee0d9)
    References: bsc#1271635
    Signed-off-by: Dario Faggioli <dfaggioli@suse.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -8319,9 +8319,6 @@ void x86_cpu_expand_features(X86CPU *cpu, Error **errp)
     }
 
     if (!cpu->enable_pmu) {
-        mark_unavailable_features(cpu, FEAT_1_ECX,
-                                  env->user_features[FEAT_1_ECX] & CPUID_EXT_PDCM,
-                                  "This feature is not available due to PMU being disabled");
         env->features[FEAT_1_ECX] &= ~CPUID_EXT_PDCM;
     }
 
