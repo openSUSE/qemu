openSUSE/qemu

commit a0274ebbdf516e6abbb28396ca271d2f92a0432d

[openSUSE][RPM] spec: Require ipxe and virtio-gpu packages for more arch-es (bsc#1240157)
qemu-imxe and some *-virtio-gpu-* packages are necessary for ARM, PPC
and s390 to work.

References: bsc#1240157
Signed-off-by: Dario Faggioli <dfaggioli@suse.com>

rpm/qemu.spec
@@ -242,8 +242,13 @@ Requires:       user(qemu)
 Requires:       (qemu-guest-agent = %{version} if qemu-guest-agent)
 ## Packages we will RECOMMEND
 %ifarch s390x
-Recommends:     qemu-hw-s390x-virtio-gpu-ccw
+Requires:     qemu-hw-s390x-virtio-gpu-ccw
 %else
+%ifarch arm
+Requires:       qemu-hw-display-virtio-gpu-pci
+%else
+Recommends:     qemu-hw-display-virtio-gpu-pci
+%endif
 Recommends:     qemu-hw-display-qxl
 Recommends:     qemu-hw-display-virtio-gpu
 Recommends:     qemu-hw-display-virtio-gpu-pci
@@ -1124,8 +1129,8 @@ This package provides i386 and x86_64 emulation.
 Summary:        Machine emulator and virtualizer for Power architectures
 Group:          System/Emulators/PC
 Requires:       %name = %{version}
+Requires:       qemu-ipxe
 Requires:       qemu-SLOF
-Recommends:     qemu-ipxe
 Recommends:     qemu-vgabios
 
 %description ppc
@@ -1166,8 +1171,8 @@ This package provides s390x emulation.
 Summary:        Machine emulator and virtualizer for ARM architectures
 Group:          System/Emulators/PC
 Requires:       %name = %{version}
+Requires:       qemu-ipxe
 Recommends:     ovmf
-Recommends:     qemu-ipxe
 Recommends:     qemu-uefi-aarch64
 Recommends:     qemu-vgabios
 

commit b8452ee356e59cfccc35454cb98efed143ee1571

[openSUSE][RPM]: really fix *-virtio-gpu-pci dependency on ARM (bsc#1254286)
Commit a0274ebbdf51 ("[openSUSE][RPM] spec: Require ipxe and virtio-gpu
packages for more arch-es (bsc#1240157)") wanted to make (among other
things) qemu-hw-display-virtio-gpu-pci an hard dependency on ARM,
but failed at it, due to a wrong macro. Fix that

Fixes: a0274ebbdf516e6abbb28396ca271d2f92a0432d
References: bsc#1254286
Signed-off-by: Dario Faggioli <dfaggioli@suse.com>

rpm/qemu.spec
@@ -244,7 +244,7 @@ Requires:       (qemu-guest-agent = %{version} if qemu-guest-agent)
 %ifarch s390x
 Recommends:     qemu-hw-s390x-virtio-gpu-ccw
 %else
-%ifarch arm
+%ifarch %{arm}
 Requires:       qemu-hw-display-virtio-gpu-pci
 %else
 Recommends:     qemu-hw-display-virtio-gpu-pci